34cee8f394f9dc264d61f952c39fea87584d0c5e
Antoine <antoine.cremel@protonmail.com>
2019-03-19 13:44:48 +0100

    Corrected indents

--- support_classes.py
+++ support_classes.py
@@ -14,28 +14,28 @@ class Loan():
 	def __init__(self, start_date, end_date,
 				 debtor, interest_rate, value, loan_type="basic"):
 		# Start by checking the compatibility of the parameters
-        
+		
 		self.start_date = start_date
 		self.end_date = end_date
 		self.debtor = debtor
 		self.interest_rate = interest_rate
 		self.value = value
 		self.type = loan_type
-        self.nb_month = ((end_date.year - start_date.year)*12)+end_date.month - start_date.month 
-        self.mensuality = value/nb_months + ((value/nb_month)*interest_rate)
-        self.remaining_val_with_interest = mensuality*nb_month
+		self.nb_month = ((end_date.year - start_date.year)*12)+end_date.month - start_date.month 
+		self.mensuality = value/nb_months + ((value/nb_month)*interest_rate)
+		self.remaining_val_with_interest = mensuality*nb_month
 
-    def __getattr__(self, name):
-    	# Definition of dynamic variables
-    	if name=="total_val":
-    		return value + value * interest_rate
+	def __getattr__(self, name):
+		# Definition of dynamic variables
+		if name=="total_val":
+			return value + value * interest_rate
 
-    	else:
-    		super().__getattr__(name)
+		else:
+			super().__getattr__(name)
 
-    def set_total_val(self, value):
-    	self.value = value + value * interest_rate
-        
+	def set_total_val(self, value):
+		self.value = value + value * interest_rate
+		
 	def get_payment(self, current_date):
 		"""
 		This function will return hoow much money is due for this month
@@ -43,40 +43,40 @@ class Loan():
 		Return : amount of money due, debtor and end.
 			- Amount of money due is the amount the debtor should pay to
 			the owner of the loan
-            - 
+			- 
 			- Debtor is the number of the agent who should pay this amount
 			- end is a boolean which says wether or not this loan can be deleted
 				from the database
 		"""
 		if self.type == "basic":
-            #if "verifier que 10% du montant de la variable value correspondant est egal ou superieur au salaire du houesehold qui contracte le pret"
+			#if "verifier que 10% du montant de la variable value correspondant est egal ou superieur au salaire du houesehold qui contracte le pret"
 			"""
 			In the case of a basic loan, everything is paid at once at
 			the end date
 			"""
 			if current_date >= end_date:
-                
+				
 				return value + (interest_rate) * value,
 					self.debtor,
 					True
 
 			
-           
-        if self.type == "household loan":
-            while current_date <= end_date:
-               
-                    return self.remainig_val_with_interest -= mensuality
-                           self.debtor,
-                           False
-                    
-                else:   
-                    
-                    return self.remainig_val_with_interest,
-                           self.debtor,
-                           True
-                    
-                else:
-                    return None, None, False       
+		   
+		if self.type == "household loan":
+			while current_date <= end_date:
+			   
+					return self.remainig_val_with_interest -= mensuality
+						   self.debtor,
+						   False
+					
+				else:   
+					
+					return self.remainig_val_with_interest,
+						   self.debtor,
+						   True
+					
+				else:
+					return None, None, False       
 
 
 ###### Helper functions ######
